3e11c2978f764b8bceac7d5f5b2b3b3fb75a86ee
BlackMeteor13 <adogu001@mail.goucher.edu>
2017-04-27 21:57:47 -0400

    Week 4

---
+++ script.js
@@ -0,0 +1,4 @@
+var yearDay= 365;
+var yearOld = prompt("What is your current age?");
+var daysOld = Math.round(yearOld * yearDay);
+alert("When it was your birthday, you were " + daysOld + "days old");
