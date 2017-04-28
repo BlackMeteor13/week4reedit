20ebb95b5d779923d29f521815cbda3a3473ba2e
BlackMeteor13 <adogu001@mail.goucher.edu>
2017-04-27 20:38:57 -0400

    my week 4 assignment

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+var color = $('#list').val();
+$('body').css('background',color);
+
+
+$('$list').change(function() {
+var color = $('#list').val();
+$('body').css('background',color);
+
+
+
+
+
+
+});
+});
